039aa38b73038043f822215eb17f8109619ae8f9
liuxian496 <liuxian494@163.com>
2026-07-21 21:34:03 +0800

    build: 升级eslint，配置import sort

---
+++ eslint.config.js
@@ -0,0 +1,102 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
+import js from '@eslint/js';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import importX from 'eslint-plugin-import';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      import: importX,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.cts', '.mts'],
+      },
+      'import/resolver-next': [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: './tsconfig.app.json',
+        }),
+      ],
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.app.json',
+        },
+      },
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'import/order': [
+        'warn',
+        {
+          warnOnUnassignedImports: true,
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'type',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '../*.stories',
+              group: 'parent',
+              position: 'before',
+            },
+            {
+              pattern: '../../pockets/**',
+              group: 'parent',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+            {
+              pattern: './*.less',
+              group: 'index',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+        },
+      ],
+    },
+  },
+  ...storybook.configs['flat/recommended'],
+]);
--- vite.config.mts
+++ vite.config.mts
@@ -15,7 +15,17 @@ const dirname =
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), visualizer()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src',
+      include: ['src/index.ts', 'src/components'],
+      outDir: 'dist',
+      insertTypesEntry: true,
+      tsconfigPath: path.join(dirname, 'tsconfig.app.json'),
+    }),
+    visualizer(),
+  ],
   build: {
     copyPublicDir: false,
     cssCodeSplit: true,
