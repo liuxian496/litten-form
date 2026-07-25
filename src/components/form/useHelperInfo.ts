4a1747b2efc03098444115f5998750a037384303
liuxian496 <liuxian494@163.com>
2026-07-25 21:11:11 +0800

    refactor: 重构verifyFormItem

--- src/components/form/useHelperInfo.ts
+++ src/components/form/useHelperInfo.ts
@@ -4,9 +4,8 @@ import { formInjector } from '../inject';
 import { warn } from '../util';
 
 import { commonValidationAssertNotFound } from './entries';
-import type { FormItemValidation, FormItemValue } from './form.types';
+import type { FormItemValidation } from './form.types';
 import { BaseValidation } from './formBase';
-
 
 /**
  * 提供一个自定义hook，管理表单项的帮助信息
@@ -23,37 +22,38 @@ import { BaseValidation } from './formBase';
 export function useHelperInfo<T, V, VT>(validations: FormItemValidation<VT>[]) {
   const [currentHelperText, setCurrentHelperText] = useState<T>();
 
-  function verifyFormItem(value: V) {
+  function verifyFormItem(value: V): T | undefined {
     let result: T | undefined;
+    let invalid = false;
     const max = validations.length;
 
     for (let i = 0; i < max; i++) {
       const { helpInfo: info, type, validationAssert } = validations[i];
 
-      // 优先使用验证规则中提供的帮助信息，如果没有，则尝试从 formInjector 获取默认帮助信息
-      const helpInfo =
-        info ??
-        (formInjector.getDefaultHelperInfo
-          ? formInjector.getDefaultHelperInfo(type)
-          : undefined);
-
       if (type === BaseValidation.Customize) {
-        result =
-          validationAssert?.(value as FormItemValue) === false
-            ? (helpInfo as T)
-            : undefined;
+        if (validationAssert !== undefined) {
+          invalid = validationAssert(value) === false;
+        } else {
+          warn('validationAssert is Undefined');
+          break;
+        }
       } else if (formInjector.commonValidationAssert) {
-        result =
-          formInjector.commonValidationAssert(value as FormItemValue, type) ===
-          false
-            ? (helpInfo as T)
-            : undefined;
+        invalid = formInjector.commonValidationAssert(value, type) === false;
       } else {
         // 没有提供通用的验证断言函数，无法进行验证，发出警告
         warn(commonValidationAssertNotFound());
+        break;
       }
 
-      if (result !== undefined) {
+      if (invalid) {
+        // 优先使用验证规则中提供的帮助信息，如果没有，则尝试从 formInjector 获取默认帮助信息
+        const helpInfo =
+          info ??
+          (formInjector.getDefaultHelperInfo
+            ? formInjector.getDefaultHelperInfo(type)
+            : undefined);
+
+        result = helpInfo as T;
         break;
       }
     }
