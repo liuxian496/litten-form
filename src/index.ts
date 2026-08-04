5aa3e89e6834139a3a5af2f89078a5b2a884479a
liuxian496 <liuxian494@163.com>
2026-08-04 22:09:57 +0800

    feat: 添加外部需要的类型导出

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,21 @@ export { useForm } from './components/form/useForm';
 export { useFormItemValue } from './components/form/useFormItemValue';
 export { useHelperInfo } from './components/form/useHelperInfo';
 export { initLittenForm } from './components/inject';
+export {
+  BaseValidationType,
+  ValidationMode,
+} from './components/form/form.types';
 //#endregion
+
 export { Mounter } from './components/mounter';
+
+//#region --- form types ---
+export type {
+  FormProps,
+  FormRef,
+  FormHelperInfo,
+  FormItemProps,
+} from './components/form/form.types';
+
+export type { MounterProps } from './components/mounter/mounter.types';
+//#endregion
